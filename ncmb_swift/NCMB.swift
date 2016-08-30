ca71772d17e90f7805056075ebe5dc4e3105eb64
RaccoonDog <RaccoonDog@example.com>
2016-08-30 15:34:35 +0900

    add the sdk version number  0.1.0

--- ncmb_swift/NCMB.swift
+++ ncmb_swift/NCMB.swift
@@ -8,6 +8,7 @@ import UIKit
 
 public class NCMB: NSObject {
 
+    public static let sdkVersion = "0.1.0"
     public static var applicationKey: String  = ""
     public static var clientKey: String  = ""
     
