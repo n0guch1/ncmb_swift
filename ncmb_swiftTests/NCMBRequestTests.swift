0c3e85078e5ee747ed828ef58ab46573532dc534
RaccoonDog <RaccoonDog@example.com>
2016-09-07 16:47:10 +0900

    fix test code for indent

--- ncmb_swiftTests/NCMBRequestTests.swift
+++ ncmb_swiftTests/NCMBRequestTests.swift
@@ -19,26 +19,20 @@ class NCMBRequestTests: XCTestCase {
         super.tearDown()
     }
     
-    // 非同期 POST
-    func testQueryConnection() {
+    func testCreateSignature() {
+        // ドキュメント公開用APIKey
+        NCMB.initializeKey("6145f91061916580c742f806bab67649d10f45920246ff459404c46f00ff3e56",
+                           clientKey: "1343d198b510a0315db1c03f3aa0e32418b7a743f8e4b47cbff670601345cf75")
+        
         let url = NSURL(string: "https://mb.api.cloud.nifty.com/2013-09-01/classes/TestClass")!
-        let request = NCMBRequest(URL: url)
-        request.HTTPMethod = "GET"
+        let reqest = NCMBRequest(URL: url)
+        reqest.HTTPMethod = "GET"
+        reqest.HTTPTimestamp = "2013-12-02T02:44:35.452Z"
         let query = NCMBQuery()
-        query.whereEqualTo("key", value: "value")
-        query.whereLessThan("score", value: 10)
-        request.HTTPQuery = query
-        request.HTTPSignature = request.createSignature()
-        let connectionExpectation: XCTestExpectation? = self.expectationWithDescription("connection test")
-        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
-            // データ取得
-            let myData:NSString = NSString(data: data!, encoding: NSUTF8StringEncoding)!
-            print("結果:",myData)
-            // 非同期完了
-            connectionExpectation?.fulfill()
-        }
-        // 非同期待ち
-        waitForExpectationsWithTimeout(10.0, handler: nil)
+        query.whereEqualTo("testKey", value: "testValue")
+        reqest.HTTPQuery = query
+        
+        XCTAssertEqual("/mQAJJfMHx2XN9mPZ9bDWR9VIeftZ97ntzDIRw0MQ4M=", reqest.createSignature())
     }
     
     // 非同期 POST
@@ -50,16 +44,17 @@ class NCMBRequestTests: XCTestCase {
         request.HTTPBody = "{\"key\":\"value\"}".dataUsingEncoding(NSUTF8StringEncoding)
         let connectionExpectation: XCTestExpectation? = self.expectationWithDescription("connection test")
         NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
+            XCTAssertNil(error)
             // データ取得
             let myData:NSString = NSString(data: data!, encoding: NSUTF8StringEncoding)!
-            print("データ確認:",myData)
+            XCTAssertNotNil(myData)
             // 非同期完了
             connectionExpectation?.fulfill()
         }
         // 非同期待ち
         waitForExpectationsWithTimeout(10.0, handler: nil)
     }
-
+    
     // 同期 GET
     func testSyncConnection() {
         let url = NSURL(string: "https://mb.api.cloud.nifty.com/2013-09-01/classes/TestClass")!
@@ -80,19 +75,23 @@ class NCMBRequestTests: XCTestCase {
         XCTAssertNotNil(myData)
     }
     
-    func testCreateSignature() {
-        // ドキュメント公開用APIKey
-        NCMB.initializeKey("6145f91061916580c742f806bab67649d10f45920246ff459404c46f00ff3e56",
-                           clientKey: "1343d198b510a0315db1c03f3aa0e32418b7a743f8e4b47cbff670601345cf75")
-    
+    func testQueryConnection() {
         let url = NSURL(string: "https://mb.api.cloud.nifty.com/2013-09-01/classes/TestClass")!
-        let reqest = NCMBRequest(URL: url)
-        reqest.HTTPMethod = "GET"
-        reqest.HTTPTimestamp = "2013-12-02T02:44:35.452Z"
+        let request = NCMBRequest(URL: url)
+        request.HTTPMethod = "GET"
         let query = NCMBQuery()
-        query.whereEqualTo("testKey", value: "testValue")
-        reqest.HTTPQuery = query
-        
-        XCTAssertEqual("/mQAJJfMHx2XN9mPZ9bDWR9VIeftZ97ntzDIRw0MQ4M=", reqest.createSignature())
+        query.whereEqualTo("key", value: "value")
+        request.HTTPQuery = query
+        request.HTTPSignature = request.createSignature()
+        let connectionExpectation: XCTestExpectation? = self.expectationWithDescription("connection test")
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
+            XCTAssertNil(error)
+            
+            let myData:NSString = NSString(data: data!, encoding: NSUTF8StringEncoding)!
+            XCTAssertNotNil(myData)
+            connectionExpectation?.fulfill()
+        }
+        waitForExpectationsWithTimeout(10.0, handler: nil)
     }
+
 }
